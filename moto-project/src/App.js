adf4b5e9bde664e3a4002c51fc49c527402d039d
Andrey Nikitin <81686981+tamga05@users.noreply.github.com>
2021-12-08 21:27:47 +0300

    adding a container

--- moto-project/src/App.js
+++ moto-project/src/App.js
@@ -5,24 +5,20 @@ import './App.css';
 import { BrowserRouter } from 'react-router-dom';
 import Content from './components/content/Content';
 import Advise from './components/content/advise/Advise';
+import Header from './components/header/Header';
 
 function App() {
 
 
   return (
     <BrowserRouter>
-<<<<<<< HEAD
-        <div className="App">
-            <Content/>
-            <Advise/>
-       </div>
-=======
       <div className="App">
-        <Header />
-        <Content />
-        <Advise />
+        <div className='container'>
+          <Header />
+          <Content />
+          <Advise />
+        </div>
       </div>
->>>>>>> f6445658d12a2e494117ac8893bf951199dc9b1a
 
     </BrowserRouter>
   );
