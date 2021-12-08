81bd803d73f065d4141714672de5cf01e5cba5d7
Andrey Nikitin <81686981+tamga05@users.noreply.github.com>
2021-12-08 22:00:31 +0300

    app change

--- moto-project/src/App.js
+++ moto-project/src/App.js
@@ -6,10 +6,7 @@ import { BrowserRouter } from 'react-router-dom';
 import Content from './components/content/Content';
 import Advise from './components/content/advise/Advise';
 import Header from './components/header/Header';
-<<<<<<< HEAD
-=======
 import Recommended from './components/recommended/Recommended';
->>>>>>> a425c30bd47c50ab1bfdf284a82e25d0acba3206
 
 function App() {
 
@@ -17,18 +14,12 @@ function App() {
   return (
     <BrowserRouter>
       <div className="App">
-<<<<<<< HEAD
-        <div className='container'>
+        <div className="container">
           <Header />
           <Content />
+          <Recommended/>
           <Advise />
         </div>
-=======
-        <Header />
-        <Content />
-        <Recommended/>
-        <Advise />
->>>>>>> a425c30bd47c50ab1bfdf284a82e25d0acba3206
       </div>
 
     </BrowserRouter>
