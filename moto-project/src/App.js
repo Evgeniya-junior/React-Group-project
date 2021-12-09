458af3a49188f398285993caf32d7e003ddd0542
Evgeniya-junior <evgeniyakravcevich@gmail.com>
2021-12-09 12:08:54 +0300

    add filter block structure

--- moto-project/src/App.js
+++ moto-project/src/App.js
@@ -1,8 +1,10 @@
-
+import * as React from 'react';
+import ReactDOM from 'react-dom';
 import './App.css';
 import { BrowserRouter } from 'react-router-dom';
 import Content from './components/content/Content';
 import Advise from './components/content/advise/Advise';
+import ContentCatalog from './components/content-Catalog/ContentCatalog';
 
 function App() {
  
@@ -10,9 +12,10 @@ function App() {
   return (
     <BrowserRouter>
         <div className="App">
-            <Content/>
+            <Content/> 
+            <ContentCatalog/>
             <Advise/>
-       </div>
+        </div>
         
     </BrowserRouter> 
   );
--- moto-project/src/components/content/sale-baner/SaleBaner.jsx
+++ moto-project/src/components/content/sale-baner/SaleBaner.jsx
@@ -13,7 +13,7 @@ function SaleBaner(props) {
                 <img className={s.imgAll} src={props.data5.img} alt="" />
             </div>
             <div className={s.saleText}>
-                <p className={s.text}>CКИДКИ <br/>на все запчасти<br/> до 70%</p>
+                <p className={s.text}>CКИДКИ <br className={s.br}/>на все запчасти<br className={s.br}/> до 70%</p>
             </div>
             <div className={s.saleButton}>
                 <button className={s.button}>ПОСМОТЕТЬ ВСЕ</button>
