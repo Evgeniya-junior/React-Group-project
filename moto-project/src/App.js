5ec9ba4c3331e743adbb0f370c85fe64746aaa45
PeterBorisenok <peterborisenok@gmail.com>
2021-12-09 11:55:31 +0300

    app test

--- moto-project/src/App.js
+++ moto-project/src/App.js
@@ -5,6 +5,7 @@ import './App.css';
 import { BrowserRouter } from 'react-router-dom';
 import Content from './components/content/Content';
 import Advise from './components/content/advise/Advise';
+import Header from './components/header/Header'
 
 function App() {
 
