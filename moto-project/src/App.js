a89bbf3ef8e6c27a6315786b23b6a70501fe45df
Andrey Nikitin <81686981+tamga05@users.noreply.github.com>
2021-12-18 14:13:16 +0300

    added links

--- moto-project/src/App.js
+++ moto-project/src/App.js
@@ -1,29 +1,28 @@
-import './App.css';
-import './index.css';
+import "./App.css";
+import "./index.css";
 
-import { BrowserRouter } from 'react-router-dom';
-import Content from './components/content/Content';
-import Advise from './components/content/advise/Advise';
-import Header from './components/header/Header';
-import Recommended from './components/recommended/Recommended';
-import ContentCatalog from './components/content-Catalog/ContentCatalog';
-import ContentProduct from './components/content-product/ContentProduct';
+import { BrowserRouter } from "react-router-dom";
+
+import Content from "./components/content/Content";
+import Advise from "./components/content/advise/Advise";
+import Header from "./components/header/Header";
+import Recommended from "./components/recommended/Recommended";
+import ContentCatalog from "./components/content-Catalog/ContentCatalog";
+import ContentProduct from "./components/content-product/ContentProduct";
 
 function App() {
-
   return (
     <BrowserRouter>
       <div className="App">
         <div className="container">
           <Header />
           <Content />
-          <ContentCatalog/>
-          <ContentProduct/>
+          <ContentCatalog />
+          <ContentProduct />
           <Recommended />
           <Advise />
         </div>
       </div>
-
     </BrowserRouter>
   );
 }
