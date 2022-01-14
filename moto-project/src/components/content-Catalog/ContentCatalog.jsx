4c5d33790630193e8a0704303aede357aa6501ea
SPashaS <SPavel.LD@gmail.com>
2022-01-14 21:32:33 +0300

    last commit fix

--- moto-project/src/components/content-Catalog/ContentCatalog.jsx
+++ moto-project/src/components/content-Catalog/ContentCatalog.jsx
@@ -25,7 +25,7 @@ import img11 from './../../assets/images/catalog-card/image/11.jpg';
 import img12 from './../../assets/images/catalog-card/image/12.jpg';
 
 
-const productMiniCard = [
+const dataCatalogCards = [
     ["Гидроцикл BRP SeaDoo GTI 130hp SE Black / Mango",img01,"1 049 500 ₽"],
     ["Гидроцикл BRP SeaDoo GTI 155hp SE Long Blue Metallic",img02,"1 100 475 ₽"],
     ["Гидроцикл BRP SeaDoo GTR 230hp X California green",img03,"1 323 700 ₽"],
@@ -60,53 +60,53 @@ return (
                 <div className={s.catalogBody}>
                     <div className={s.catalogContent}>
                         <CatalogCard 
-                            title={productMiniCard[0][0]} 
-                            image={productMiniCard[0][1]}
-                            price={productMiniCard[0][2]}/>
+                            title={dataCatalogCards[0][0]} 
+                            image={dataCatalogCards[0][1]}
+                            price={dataCatalogCards[0][2]}/>
                         <CatalogCard 
-                            title={productMiniCard[1][0]} 
-                            image={productMiniCard[1][1]}
-                            price={productMiniCard[1][2]}/>
+                            title={dataCatalogCards[1][0]} 
+                            image={dataCatalogCards[1][1]}
+                            price={dataCatalogCards[1][2]}/>
                         <CatalogCard 
-                            title={productMiniCard[2][0]} 
-                            image={productMiniCard[2][1]}
-                            price={productMiniCard[2][2]}/>
+                            title={dataCatalogCards[2][0]} 
+                            image={dataCatalogCards[2][1]}
+                            price={dataCatalogCards[2][2]}/>
                         <CatalogCardOut 
-                            title={productMiniCard[3][0]} 
-                            image={productMiniCard[3][1]}
-                            price={productMiniCard[3][2]}/>
+                            title={dataCatalogCards[3][0]} 
+                            image={dataCatalogCards[3][1]}
+                            price={dataCatalogCards[3][2]}/>
                         <CatalogCard 
-                            title={productMiniCard[4][0]} 
-                            image={productMiniCard[4][1]}
-                            price={productMiniCard[4][2]}/>
+                            title={dataCatalogCards[4][0]} 
+                            image={dataCatalogCards[4][1]}
+                            price={dataCatalogCards[4][2]}/>
                         <CatalogCard 
-                            title={productMiniCard[5][0]} 
-                            image={productMiniCard[5][1]}
-                            price={productMiniCard[5][2]}/>
+                            title={dataCatalogCards[5][0]} 
+                            image={dataCatalogCards[5][1]}
+                            price={dataCatalogCards[5][2]}/>
                         <CatalogCard 
-                            title={productMiniCard[6][0]} 
-                            image={productMiniCard[6][1]}
-                            price={productMiniCard[6][2]}/>
+                            title={dataCatalogCards[6][0]} 
+                            image={dataCatalogCards[6][1]}
+                            price={dataCatalogCards[6][2]}/>
                         <CatalogCard 
-                            title={productMiniCard[7][0]} 
-                            image={productMiniCard[7][1]}
-                            price={productMiniCard[7][2]}/>
+                            title={dataCatalogCards[7][0]} 
+                            image={dataCatalogCards[7][1]}
+                            price={dataCatalogCards[7][2]}/>
                         <CatalogCard 
-                            title={productMiniCard[8][0]} 
-                            image={productMiniCard[8][1]}
-                            price={productMiniCard[8][2]}/>
+                            title={dataCatalogCards[8][0]} 
+                            image={dataCatalogCards[8][1]}
+                            price={dataCatalogCards[8][2]}/>
                         <CatalogCard 
-                            title={productMiniCard[9][0]} 
-                            image={productMiniCard[9][1]}
-                            price={productMiniCard[9][2]}/>
+                            title={dataCatalogCards[9][0]} 
+                            image={dataCatalogCards[9][1]}
+                            price={dataCatalogCards[9][2]}/>
                         <CatalogCardOut
-                            title={productMiniCard[10][0]} 
-                            image={productMiniCard[10][1]}
-                            price={productMiniCard[10][2]}/>
+                            title={dataCatalogCards[10][0]} 
+                            image={dataCatalogCards[10][1]}
+                            price={dataCatalogCards[10][2]}/>
                         <CatalogCardOut
-                            title={productMiniCard[11][0]} 
-                            image={productMiniCard[11][1]}
-                            price={productMiniCard[11][2]}/>
+                            title={dataCatalogCards[11][0]} 
+                            image={dataCatalogCards[11][1]}
+                            price={dataCatalogCards[11][2]}/>
                     </div>
                     <div className={s.catalogFooter}>
                         <Pagination count={11} variant="outlined" shape="rounded" color='primary' />
