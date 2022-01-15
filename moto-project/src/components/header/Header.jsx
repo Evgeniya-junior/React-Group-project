8e41aceaf162f1c85269e29adbff0aeeaf6a04f1
SPashaS <SPavel.LD@gmail.com>
2022-01-16 01:43:27 +0300

    new header start

--- moto-project/src/components/header/Header.jsx
+++ moto-project/src/components/header/Header.jsx
@@ -1,67 +1,79 @@
 import React from "react";
 import { NavLink } from 'react-router-dom';
 import s from "./Header.module.scss";
-import HeaderLink from "./HeaderLink";
-import logo from "./../../assets/img/logo-icon.svg";
-import location from "./../../assets/img/location-icon.svg";
-import personal from "./../../assets/img/personal-icon.svg";
-import Heart from './Heart';
-import Basket from './Basket';
-import Menu from "./Menu";
-import BasketCounter from "./BasketCounter";
 
-
-
+import favorite from './../../assets/images/icon-svg/favorite.svg';
+import user from './../../assets/images/icon-svg/user.svg';
+import cart from './../../assets/images/icon-svg/cart.svg';
+import logo from './../../assets/images/icon-svg/logo.svg';
+import HeaderMenuItem from "./header-menu-item/HeaderMenuItem";
+import MenuItem from "./menu-item/MenuItem";
 
 
 
 function Header() {
 
    return (
-      <>
-         <header className={s.header}>
-            <nav className={s.menu}>
-               <ul className={s.menuList}>
-                  <li className={s.menuItem}>
-                     <HeaderLink href="#" text="Магазины" />
-                  </li>
-                  <li className={s.menuItem}>
-                     <HeaderLink href="#" text="Акции" />
-                  </li>
-                  <li className={s.menuItem}>
-                     <HeaderLink href="#" text="Доставка и оплата" />
-                  </li>
+      <header className={s.header}>
+         <div className={s.headerBody}>
+            <button type="button" class={s.iconMenu}><span></span></button>
+            <nav class={s.menuBody}>
+               <ul class={s.menuList}>
+                  <MenuItem text="Магазины" link="/shops"/>
+                  <MenuItem text="Акции" link="/sales"/>
+                  <MenuItem text="Доставка и оплата" link="/delivery"/>
                </ul>
             </nav>
-
-            <NavLink to="/" className={s.logo} ><img className="logoImg" src={logo} alt="#" /></NavLink>
-
-            <nav className={s.social}>
-               <address className={s.adress}>
-                  <img src={location} alt="location" />
-                  <HeaderLink href="#" text="Москва,  ул. Науки  25" style={{ margin: "0" }} />
-               </address>
-               <nav className={s.socialIcons}>
-                  <ul className={s.socialList}>
-                     <li className={s.socialItem}>
-                        <Heart />
+            <div class={s.logo}>
+               <a  class={s.logoLink} href="">
+                  <img class={s.logoImage} src={logo} alt="" />
+                  <span class={s.logoText}>DRIVE MOTO</span>
+               </a>
+            </div>
+            <div className={s.rightBlock}>
+               <div href="" className={s.location}>
+                  <a href="" className={s.locationLink}>
+                     <span>Москва,  ул. Науки  25</span>
+                  </a>
+               </div>
+               
+               <div className={s.actions}>
+                  <ul className={s.actionsList}>
+                     <li className={s.actionsItem}>
+                        <a href="" className={s.favorite}>
+                           <img src={favorite} alt="" />
+                        </a>
                      </li>
-                     <li className={s.socialItem}>
-                        <img src={personal} alt="" />
+                     <li className={s.actionsItem}>
+                        <a href="" className={s.user}>
+                           <img src={user} alt="" />
+                        </a>
                      </li>
-                     <li className={s.socialItem}>
-                        <Basket />
-                        <BasketCounter text="1" />
+                     <li className={s.actionsItem}>
+                        <a href="" className={s.cart}>
+                           <img src={cart} alt="" />
+                           <span className={s.cartCount}>1</span>
+                        </a>
                      </li>
                   </ul>
-               </nav>
+               </div>
+            </div>
+         </div>
+         <div className={s.headerMenu}>
+            <nav className={s.headerMenuBody}>
+               <ul className={s.headerMenuList}>
+                  <HeaderMenuItem text="Квадроциклы" link="/Квадроциклы"/>
+                  <HeaderMenuItem text="Катера" link="/Катера"/>
+                  <HeaderMenuItem text="Гидроциклы" link="/Гидроциклы"/>
+                  <HeaderMenuItem text="Лодки" link="/Лодки"/>
+                  <HeaderMenuItem text="Вездеходы" link="/Вездеходы"/>
+                  <HeaderMenuItem text="Снегоходы" link="/Снегоходы"/>
+                  <HeaderMenuItem text="Двигатели" link="/Двигатели"/>
+                  <HeaderMenuItem text="Запчасти" link="/Запчасти"/>
+               </ul>
             </nav>
-         </header>
-         <Menu />
-
-
-
-      </>
+         </div>
+      </header>
    );
 }
 
